89d1670a899998e00efe629533b9ce0e7305eb59
Samir_Pandey <pandesamir3@gmail.com>
2022-06-29 13:35:57 +0545

    Learning Data Structure and Algorithms

---
+++ BIG O notation/adder.js
@@ -0,0 +1,20 @@
+// function addUpto(n){
+//     let total = 0;
+//     for(let i = 1;i<=n;i++){
+//         total += i;
+//     }
+//     console.log(total)
+// }
+// let t1 = performance.now()
+// addUpto(10000000000)
+// let t2 = performance.now()
+// console.log(t2-t1/1000)
+
+
+// function addUpto(n){
+//     return n*(n+1)/2
+// }
+// let t1 = performance.now()
+// console.log(addUpto(10000000000))
+// let t2 = performance.now()
+// console.log(t2-t1)
